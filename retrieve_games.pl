acb555c61bc0986971b925b30a9d3aee65aa2458
Josh <joshuacastellano7@gmail.com>
2018-08-14 17:33:00 -0400

    retrieve_games.pl implemented.

--- retrieve_games.pl
+++ retrieve_games.pl
@@ -2,29 +2,69 @@
 
 use warnings;
 use strict;
+use Data::Dumper;
 
 sub retrieve($$)
 {
   my $name = shift;
   my $dir = shift;
+  
+  # Constants
+  my $query_url_prefix = "http://www.cross-tables.com/players.php?query=";
+  my $annotated_games_url_prefix = "http://www.cross-tables.com/anno.php?p=";
+  my $annotated_games_page_name = "anno_page.html";
+  my $query_results_page_name = "query_results.html";
 
-  my $results_page_name = "anno_page.html";
+  # Prepare name for cross-tables query
+  # by replacing ' ' with '+'
+  my $user_name = $name;
+  $name =~ s/ /+/g;
 
-  # name -> url
+  # Run a query using the name to get the player id
+  my $query_url = $query_url_prefix . $name;
+  open (CMDOUT, "wget $query_url -O ./$query_results_page_name 2>&1 |");
+  my $player_id;
+  while (<CMDOUT>)
+  {
+  	/^Location: results\.php\?p=(\d+).*/;
+  	$player_id = $1;
+  	if ($player_id)
+  	{
+  	  last;
+  	}
+  }
+  if (!$player_id){die "No player named $user_name found";}
+  # Use the player id to get the address of the annotated games page
+  my $annotated_games_url = $annotated_games_url_prefix . $player_id;
 
-  my $url = 'http://www.cross-tables.com/anno.php?p=20411';
+  # Get the player's annotated games page
+  system "wget $annotated_games_url -O ./$annotated_games_page_name";
+
+  # Parse the annotated games page html to get the game ids
+  # of the player's annotated games
+  my @game_ids = ();
+  open(RESULTS, '<', $annotated_games_page_name);
+  while (<RESULTS>)
+  {
+    my @matches = ($_ =~ /<a href='annotated\.php\?u=(\d+)'>/g);
+    push @game_ids, @matches;
+  }
+
+  # Create the directory to store the game page htmls
+  # if one doesn't already exist
   if (!(-e $dir and -d $dir))
   {
   	mkdir $dir;
   }
-  system "wget $url -O ./$results_page_name";
-  open(RESULTS, '<', $results_page_name);
-  while (<RESULTS>)
+
+  # Iterate through the annotated game ids to fetch all of
+  # the player's annotated games
+  while (@game_ids)
   {
-
+  	my $id = shift @game_ids;
+  	my $game_url = "http://www.cross-tables.com/annotated.php?u=$id";
+  	my $game_name = "$id.html";
+  	system "wget $game_url -O $dir/$game_name";
   }
-
 }
-
-
 1;
