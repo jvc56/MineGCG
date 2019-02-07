c4e79229cdec23d28cdde8b846ae4fbdb4282a1f
Josh <joshuacastellano7@gmail.com>
2019-02-06 23:47:52 -0500

    Updated web app mine

--- scripts/webapp_main.pl
+++ scripts/webapp_main.pl
@@ -13,13 +13,19 @@ my $name;
 my $cort    = '';
 my $game    = '';
 my $tid     = '';
+my $opp     = '';
+my $start   = '';
+my $end     = '';
 
 GetOptions (
             'name=s'    => \$name,
             'cort:s'    => \$cort,
             'game:s'    => \$game,
             'tid:s'     => \$tid,
+            'opp:s'     => \$opp,
+            '$start:s'  => \$start,
+            '$end:s'    => \$end
            );
 
-mine(sanitize($name), $cort, $game, 1, $tid, 1);
+mine(sanitize($name), $cort, $game, $opp, $start, $end, 1, $tid, 1);
 
