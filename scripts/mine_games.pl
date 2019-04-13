859a9e9af01e00cc0e8f1e99cc7a77d83de5154a
Josh <joshuacastellano7@gmail.com>
2019-04-13 11:35:06 -0400

    Removed debug code

--- scripts/mine_games.pl
+++ scripts/mine_games.pl
@@ -392,10 +392,6 @@ sub mine
       if ($type eq Constants::STAT_ITEM_LIST_NOTABLE)
       {
         print $fh $e->{'name'} . ": ";
-        if (!($e->{'list'}))
-        {
-          print STDERR "\nUndefined List for: $player_name\n";
-        }
         my @list = @{$e->{'list'}};
         for (my $i = 0; $i < scalar @list; $i++)
         {
