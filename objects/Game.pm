e3c3a4299428fe68006893aad9d9fe6bd675b1b7
Josh <joshuacastellano7@gmail.com>
2019-05-28 14:16:28 -0400

    Included breakdown other every time. Might revert later

--- objects/Game.pm
+++ objects/Game.pm
@@ -419,24 +419,64 @@ sub new
     '0' =>
       {
         'total' => 0,
-        '?' => 0,
-        'Z' => 0,
-        'Q' => 0,
-        'X' => 0,
-        'J' => 0,
-        'S' => 0,
+        'A' => 0,
+        'B' => 0,
+        'C' => 0,
+        'D' => 0,
         'E' => 0,
+        'F' => 0,
+        'G' => 0,
+        'H' => 0,
+        'I' => 0,
+        'J' => 0,
+        'K' => 0,
+        'L' => 0,
+        'M' => 0,
+        'N' => 0,
+        'O' => 0,
+        'P' => 0,
+        'Q' => 0,
+        'R' => 0,
+        'S' => 0,
+        'T' => 0,
+        'U' => 0,
+        'V' => 0,
+        'W' => 0,
+        'X' => 0,
+        'Y' => 0,
+        'Z' => 0,
+        '?' => 0
       },
     '1' =>
       {
         'total' => 0,
-        '?' => 0,
-        'Z' => 0,
-        'Q' => 0,
-        'X' => 0,
-        'J' => 0,
-        'S' => 0,
+        'A' => 0,
+        'B' => 0,
+        'C' => 0,
+        'D' => 0,
         'E' => 0,
+        'F' => 0,
+        'G' => 0,
+        'H' => 0,
+        'I' => 0,
+        'J' => 0,
+        'K' => 0,
+        'L' => 0,
+        'M' => 0,
+        'N' => 0,
+        'O' => 0,
+        'P' => 0,
+        'Q' => 0,
+        'R' => 0,
+        'S' => 0,
+        'T' => 0,
+        'U' => 0,
+        'V' => 0,
+        'W' => 0,
+        'X' => 0,
+        'Y' => 0,
+        'Z' => 0,
+        '?' => 0
       }
   };
 
@@ -1413,33 +1453,13 @@ sub postConstruction
         {
           $total_tiles_played++;
           $this->{'tiles_played'}->{$turn}->{'total'}++;
-          if ($c eq 'E')
-          {
-            $this->{'tiles_played'}->{$turn}->{'E'}++;
-          }
-          elsif ($c eq 'Z')
-          {
-            $this->{'tiles_played'}->{$turn}->{'Z'}++;
-          }
-          elsif ($c eq 'Q')
-          {
-            $this->{'tiles_played'}->{$turn}->{'Q'}++;
-          }
-          elsif ($c eq 'X')
-          {
-            $this->{'tiles_played'}->{$turn}->{'X'}++;
-          }
-          elsif ($c eq 'J')
-          {
-            $this->{'tiles_played'}->{$turn}->{'J'}++;
-          }
-          elsif ($c eq 'S')
-          {
-            $this->{'tiles_played'}->{$turn}->{'S'}++;
-          }        
-          elsif ($c =~ /[a-z]/)
+          if ($c =~ /[a-z]/)
           {
             $this->{'tiles_played'}->{$turn}->{'?'}++;
+          }
+          else
+          {
+            $this->{'tiles_played'}->{$turn}->{uc $c}++;
           }
         }
       }
--- objects/StatsItem.pm
+++ objects/StatsItem.pm
@@ -666,9 +666,80 @@ sub updateNumTilesPlayed
   if (!$this->{'init'})
   {
     $this->{'init'} = 1;
+    my %subitems =
+    (
+        'A' => 0,
+        'B' => 0,
+        'C' => 0,
+        'D' => 0,
+        'E' => 0,
+        'F' => 0,
+        'G' => 0,
+        'H' => 0,
+        'I' => 0,
+        'J' => 0,
+        'K' => 0,
+        'L' => 0,
+        'M' => 0,
+        'N' => 0,
+        'O' => 0,
+        'P' => 0,
+        'Q' => 0,
+        'R' => 0,
+        'S' => 0,
+        'T' => 0,
+        'U' => 0,
+        'V' => 0,
+        'W' => 0,
+        'X' => 0,
+        'Y' => 0,
+        'Z' => 0,
+        '?' => 0
+    );
+    $this->{'subitems'} = \%subitems;
+    my @order =
+    (
+        'A',
+        'B',
+        'C',
+        'D',
+        'E',
+        'F',
+        'G',
+        'H',
+        'I',
+        'J',
+        'K',
+        'L',
+        'M',
+        'N',
+        'O',
+        'P',
+        'Q',
+        'R',
+        'S',
+        'T',
+        'U',
+        'V',
+        'W',
+        'X',
+        'Y',
+        'Z',
+        '?'
+    );
+    $this->{'list'} = \@order;
   }
 
+
   $this->{'total'} += $game->{'tiles_played'}->{$this_player}->{'total'};
+
+  my $blanks = $game->{'tiles_played'}->{$this_player}->{'?'};
+  $this->{'subitems'}->{'?'} += $blanks;
+
+  foreach my $c ("A" .. "Z")
+  {
+     $this->{'subitems'}->{$c} += $game->{'tiles_played'}->{$this_player}->{$c};
+  }
 }
 
 sub updateNumPowerTilesPlayed
