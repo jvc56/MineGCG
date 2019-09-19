89ec07f9aed5067a78bac97f0dacdb7d70d0614f
Joshua Castellano <castellaj@VM-kellytg.corp.leidos.com>
2019-09-19 11:30:51 -0400

    Still lost

--- modules/Update.pm
+++ modules/Update.pm
@@ -662,83 +662,59 @@ sub update_html
 
 <body>
 
-  <!--Navbar-->
-  <nav class="navbar navbar-expand-lg navbar-dark primary-color">
-    <!-- Navbar brand -->
-    <a class="navbar-brand" href="/">RandomRacer</a>
-    <!-- Collapse button -->
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav"
-      aria-controls="basicExampleNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <!-- Collapsible content -->
-    <div class="collapse navbar-collapse" id="basicExampleNav">
-      <!-- Links -->
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="#">Player Search
-            <span class="sr-only">(current)</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Leaderboards</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Notable</a>
-        </li>
-        <!-- Dropdown -->
-        <li class="nav-item dropdown multi-level-dropdown">
-          <a href="#" id="menu" data-toggle="dropdown" class="nav-link dropdown-toggle w-100">Dropdown</a>
-          <ul class="dropdown-menu mt-2 rounded-0 primary-color border-0 z-depth-1">
-            <li class="dropdown-item dropdown-submenu p-0">
-              <a href="#" data-toggle="dropdown" class="dropdown-toggle text-white w-100">Click Me Too! </a>
-              <ul class="dropdown-menu ml-2 rounded-0 primary-color border-0 z-depth-1">
-                <li class="dropdown-item p-0">
-                  <a href="#" class="text-white w-100">Hey</a>
-                </li>
-                <li class="dropdown-item p-0">
-                  <a href="#" class="text-white w-100">Hi</a>
-                </li>
-                <li class="dropdown-item dropdown-submenu p-0">
-                  <a href="#" data-toggle="dropdown" class="dropdown-toggle text-white w-100">Hello </a>
-                  <ul class="dropdown-menu mr-2 rounded-0 primary-color border-0 z-depth-1 r-100 ">
-                    <li class="dropdown-item p-0">
-                      <a href="#" class="text-white w-100">Some</a>
-                    </li>
-                    <li class="dropdown-item p-0">
-                      <a href="#" class="text-white w-100">Text</a>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-            </li>
-            <li class="dropdown-item dropdown-submenu">
-              <a href="#" data-toggle="dropdown" class="dropdown-toggle text-white w-100">Click me </a>
-              <ul class="dropdown-menu mr-2 rounded-0  primary-color border-0 z-depth-1 r-100 ">
-                <li class="dropdown-item p-0">
-                  <a href="#" class="text-white w-100">How</a>
-                </li>
-                <li class="dropdown-item p-0">
-                  <a href="#" class="text-white w-100">are</a>
-                </li>
-                <li class="dropdown-item p-0">
-                  <a href="#" class="text-white w-100">you </a>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </li>
-      </ul>
-      <!-- Links -->
-      <form class="form-inline">
-        <div class="md-form my-0">
-          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+<!--Navbar-->
+<nav class="navbar navbar-expand-lg navbar-dark primary-color">
+
+  <!-- Navbar brand -->
+  <a class="navbar-brand" href="#">RandomRacer</a>
+
+  <!-- Collapse button -->
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav"
+    aria-controls="basicExampleNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <!-- Collapsible content -->
+  <div class="collapse navbar-collapse" id="basicExampleNav">
+
+    <!-- Links -->
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home
+          <span class="sr-only">(current)</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Leaderboards</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Notable</a>
+      </li>
+
+      <!-- Dropdown -->
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
+          aria-haspopup="true" aria-expanded="false">Dropdown</a>
+        <div class="dropdown-menu dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Something else here</a>
         </div>
-      </form>
-    </div>
-    <!-- Collapsible content -->
-  </nav>
-  <!--/.Navbar-->
+      </li>
+
+    </ul>
+    <!-- Links -->
+
+    <form class="form-inline">
+      <div class="md-form my-0">
+        <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+      </div>
+    </form>
+  </div>
+  <!-- Collapsible content -->
+
+</nav>
+<!--/.Navbar-->
 
   <!-- SCRIPTS -->
   <!-- JQuery -->
