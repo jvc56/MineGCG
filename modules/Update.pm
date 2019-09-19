ae214776f933973ef09a072e8f281c267efda817
Joshua Castellano <joshuacastellano7@gmail.com>
2019-09-19 12:46:51 +0000

    Bootstrap stuff

--- modules/Update.pm
+++ modules/Update.pm
@@ -642,179 +642,320 @@ sub update_html
   $search_data_id =~ s/\..*//g;
   my $index_html = <<HTML
 
-<script>
+<!DOCTYPE html>
+<html lang="en">
 
-function nocacheresult()
-{
-  $validation
-
-  var inputs = document.getElementsByTagName("input");
-  for (i = 0; i < inputs.length; i++)
-  {
-    if (inputs[i].value != "Submit" && inputs[i].name != "name" && inputs[i].value != "")
-    {
-      // debug.innerHTML = inputs[i].name + " -  "  + inputs[i].value;
-      return true;
-    }
-  }
-  var selects = document.getElementsByTagName("select");
-  for (i = 0; i < selects.length; i++)
-  {
-    if (selects[i].value != "")
-    {
-      // debug.innerHTML = selects[i].name + " -s-  "  + selects[i].value;
-      return true;
-    }
-  }
-
-  var name = document.getElementsByName("name")[0].value;
-
-  // Sanitize exactly as MineGCG does
-
-  name = name.trim();
-
-  name = name.replace(/ /g, "_");
-
-  name = name.replace(/[^\\w\\-]/g, "");
-
-  name = name.toUpperCase();
-
-  window.open("/cache/" + name + ".html", "_blank");
-
-  return false;
-}
-
-</script>
-<html>
 <head>
-<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
-<title>RandomRacer</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Material Design Bootstrap</title>
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+  <!-- Bootstrap core CSS -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <!-- Material Design Bootstrap -->
+  <link href="css/mdb.min.css" rel="stylesheet">
+  <!-- Your custom styles (optional) -->
 
+  <style>
 
-
-<link rel="stylesheet" href="carousel.css" />
-<script src="https://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
-
-
-<link rel="stylesheet" href="style.css" />
-<link rel=icon href=/favicon.png>
-<link href="https://fonts.googleapis.com/css?family=Francois+One|Iceland|Monofett|Orbitron|Press+Start+2P|Source+Code+Pro|VT323" rel="stylesheet">
-<!--
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
-
--->
-
-<script>
-\$(function(){
-  \$("#$search_data_id").load("$search_data_html");
-});
-
-
-\$(document).ready(function()
-{
-  \$('.customer-logos').slick(
-  {
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 1000,
-    arrows: false,
-    dots: false,
-    pauseOnHover: false,
-    responsive: 
-    [
-      {
-        breakpoint: 768,
-        settings: 
-        {
-          slidesToShow: 3
-        }
-      }, 
-      {
-        breakpoint: 520,
-        settings: 
-        {
-          slidesToShow: 2
-        }
-      }
-    ]
-  });
-});
-
-
-</script>
-
+  </style>
 
 
 </head>
-<body id="body">
 
-<div class="typingdiv">
- <table class="typingdivtable">
-  <tbody>
-   <tr>
-    <td class="webtitletd">
-     <a href="/" class="webtitle">RandomRacer</a>
-    </td>
-    <td class="externallinks">
-      <table>
-  <tbody>
-   <tr>
+<body class="fixed-sn grey-skin">
 
-   <td class="alink">
-      <a href="/leaderboard.html" title="Leaderboards">Leaderboards</a>
-   </td>
+  <!--Double navigation-->
+  <header>
+    <!-- Sidebar navigation -->
+    <div id="slide-out" class="side-nav sn-bg-4 fixed">
+      <ul class="custom-scrollbar">
+        <!-- Logo -->
+        <li>
+          <div class="logo-wrapper waves-light">
+            <a href="#"><img src="https://mdbootstrap.com/img/logo/mdb-transparent.png" class="img-fluid flex-center"></a>
+          </div>
+        </li>
+        <!--/. Logo -->
+        <!--Social-->
+        <li>
+          <ul class="social">
+            <li><a href="#" class="icons-sm fb-ic"><i class="fab fa-facebook-f"> </i></a></li>
+            <li><a href="#" class="icons-sm pin-ic"><i class="fab fa-pinterest"> </i></a></li>
+            <li><a href="#" class="icons-sm gplus-ic"><i class="fab fa-google-plus-g"> </i></a></li>
+            <li><a href="#" class="icons-sm tw-ic"><i class="fab fa-twitter"> </i></a></li>
+          </ul>
+        </li>
+        <!--/Social-->
+        <!--Search Form-->
+        <li>
+          <form class="search-form" role="search">
+            <div class="form-group md-form mt-0 pt-1 waves-light">
+              <input type="text" class="form-control" placeholder="Search">
+            </div>
+          </form>
+        </li>
+        <!--/.Search Form-->
+        <!-- Side navigation links -->
+        <li>
+          <ul class="collapsible collapsible-accordion">
+            <li><a class="collapsible-header waves-effect arrow-r"><i class="fas fa-chevron-right"></i> Submit blog<i
+                  class="fas fa-angle-down rotate-icon"></i></a>
+              <div class="collapsible-body">
+                <ul class="list-unstyled">
+                  <li><a href="#" class="waves-effect">Submit listing</a>
+                  </li>
+                  <li><a href="#" class="waves-effect">Registration form</a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li><a class="collapsible-header waves-effect arrow-r"><i class="far fa-hand-pointer"></i> Instruction<i
+                  class="fas fa-angle-down rotate-icon"></i></a>
+              <div class="collapsible-body">
+                <ul class="list-unstyled">
+                  <li><a href="#" class="waves-effect">For bloggers</a>
+                  </li>
+                  <li><a href="#" class="waves-effect">For authors</a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li><a class="collapsible-header waves-effect arrow-r"><i class="fas fa-eye"></i> About<i class="fas fa-angle-down rotate-icon"></i></a>
+              <div class="collapsible-body">
+                <ul class="list-unstyled">
+                  <li><a href="#" class="waves-effect">Introduction</a>
+                  </li>
+                  <li><a href="#" class="waves-effect">Monthly meetings</a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li><a class="collapsible-header waves-effect arrow-r"><i class="far fa-envelope"></i> Contact me<i class="fas fa-angle-down rotate-icon"></i></a>
+              <div class="collapsible-body">
+                <ul class="list-unstyled">
+                  <li><a href="#" class="waves-effect">FAQ</a>
+                  </li>
+                  <li><a href="#" class="waves-effect">Write a message</a>
+                  </li>
+                  <li><a href="#" class="waves-effect">FAQ</a>
+                  </li>
+                  <li><a href="#" class="waves-effect">Write a message</a>
+                  </li>
+                  <li><a href="#" class="waves-effect">FAQ</a>
+                  </li>
+                  <li><a href="#" class="waves-effect">Write a message</a>
+                  </li>
+                  <li><a href="#" class="waves-effect">FAQ</a>
+                  </li>
+                  <li><a href="#" class="waves-effect">Write a message</a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+          </ul>
+        </li>
+        <!--/. Side navigation links -->
+      </ul>
+      <div class="sidenav-bg mask-strong"></div>
+    </div>
+    <!--/. Sidebar navigation -->
+    <!-- Navbar -->
+    <nav class="navbar fixed-top navbar-toggleable-md navbar-expand-lg scrolling-navbar double-nav">
+      <!-- SideNav slide-out button -->
+      <div class="float-left">
+        <a href="#" data-activates="slide-out" class="button-collapse"><i class="fas fa-bars"></i></a>
+      </div>
+      <!-- Breadcrumb-->
+      <div class="breadcrumb-dn mr-auto">
+        <p>Material Design for Bootstrap</p>
+      </div>
+      <ul class="nav navbar-nav nav-flex-icons ml-auto">
+        <li class="nav-item">
+          <a class="nav-link"><i class="fas fa-envelope"></i> <span class="clearfix d-none d-sm-inline-block">Contact</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link"><i class="far fa-comments"></i> <span class="clearfix d-none d-sm-inline-block">Support</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link"><i class="fas fa-user"></i> <span class="clearfix d-none d-sm-inline-block">Account</span></a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true"
+            aria-expanded="false">
+            Dropdown
+          </a>
+          <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <a class="dropdown-item" href="#">Something else here</a>
+          </div>
+        </li>
+      </ul>
+    </nav>
+    <!-- /.Navbar -->
+  </header>
+  <!--/.Double navigation-->
+
+  <!--Main layout-->
+  <main>
+
+    <div class="container-fluid text-center">
+
+      <!--Card-->
+      <div class="card card-cascade wider reverse my-4 pb-5">
+
+        <!--Card image-->
+        <div class="view view-cascade overlay wow fadeIn">
+          <img src="https://mdbootstrap.com/img/Photos/Slides/img%20(70).jpg" class="img-fluid">
+          <a href="#!">
+            <div class="mask rgba-white-slight"></div>
+          </a>
+        </div>
+        <!--/Card image-->
+
+        <!--Card content-->
+        <div class="card-body card-body-cascade text-center wow fadeIn" data-wow-delay="0.2s">
+          <!--Title-->
+          <h4 class="card-title"><strong>My adventure</strong></h4>
+          <h5 class="blue-text"><strong>Photography</strong></h5>
+
+          <p class="card-text">Sed ut perspiciatis unde omnis iste natus sit voluptatem accusantium doloremque
+            laudantium, totam rem aperiam.
+          </p>
+
+          <a class="btn btn-primary btn-lg">Primary button</a>
+          <a class="btn btn-secondary btn-lg">Secondary button</a>
+          <a class="btn btn-default btn-lg">Default button</a>
+
+        </div>
+        <!--/.Card content-->
+
+      </div>
+      <!--/.Card-->
+
+    </div>
+
+  </main>
+  <!--/Main layout-->
+
+  <!--Footer-->
+  <footer class="page-footer text-center text-md-left pt-4">
+
+    <!--Footer Links-->
+    <div class="container-fluid">
+      <div class="row">
+
+        <!--First column-->
+        <div class="col-md-3">
+          <h5 class="text-uppercase font-weight-bold mb-4">Footer Content</h5>
+          <p>Here you can use rows and columns here to organize your footer content.</p>
+        </div>
+        <!--/.First column-->
+
+        <hr class="w-100 clearfix d-md-none">
+
+        <!--Second column-->
+        <div class="col-md-2 mx-auto">
+          <h5 class="text-uppercase font-weight-bold mb-4">Links</h5>
+          <ul class="list-unstyled">
+            <li><a href="#!">Link 1</a></li>
+            <li><a href="#!">Link 2</a></li>
+            <li><a href="#!">Link 3</a></li>
+            <li><a href="#!">Link 4</a></li>
+          </ul>
+        </div>
+        <!--/.Second column-->
+
+        <hr class="w-100 clearfix d-md-none">
+
+        <!--Third column-->
+        <div class="col-md-2 mx-auto">
+          <h5 class="text-uppercase font-weight-bold mb-4">Links</h5>
+          <ul class="list-unstyled">
+            <li><a href="#!">Link 1</a></li>
+            <li><a href="#!">Link 2</a></li>
+            <li><a href="#!">Link 3</a></li>
+            <li><a href="#!">Link 4</a></li>
+          </ul>
+        </div>
+        <!--/.Third column-->
+
+        <hr class="w-100 clearfix d-md-none">
+
+        <!--Fourth column-->
+        <div class="col-md-2 mx-auto">
+          <h5 class="text-uppercase font-weight-bold mb-4">Links</h5>
+          <ul class="list-unstyled">
+            <li><a href="#!">Link 1</a></li>
+            <li><a href="#!">Link 2</a></li>
+            <li><a href="#!">Link 3</a></li>
+            <li><a href="#!">Link 4</a></li>
+          </ul>
+        </div>
+        <!--/.Fourth column-->
+
+      </div>
+    </div>
+    <!--/.Footer Links-->
+
+    <hr>
+
+    <!--Call to action-->
+    <div class="call-to-action text-center my-4">
+      <ul class="list-unstyled list-inline">
+        <li class="list-inline-item">
+          <h5>Register for free</h5>
+        </li>
+        <li class="list-inline-item"><a href="" class="btn btn-primary">Sign up!</a></li>
+      </ul>
+    </div>
+    <!--/.Call to action-->
+
+    <hr>
+
+    <!--Social buttons-->
+    <div class="social-section text-center">
+      <ul class="list-unstyled list-inline">
+        <li class="list-inline-item"><a class="btn-floating btn-fb"><i class="fab fa-facebook-f"> </i></a></li>
+        <li class="list-inline-item"><a class="btn-floating btn-tw"><i class="fab fa-twitter"> </i></a></li>
+        <li class="list-inline-item"><a class="btn-floating btn-gplus"><i class="fab fa-google-plus-g"> </i></a></li>
+        <li class="list-inline-item"><a class="btn-floating btn-li"><i class="fab fa-linkedin-in"> </i></a></li>
+        <li class="list-inline-item"><a class="btn-floating btn-git"><i class="fab fa-github"> </i></a></li>
+      </ul>
+    </div>
+    <!--/.Social buttons-->
+
+    <!--Copyright-->
+    <div class="footer-copyright py-3 text-center">
+      <div class="container-fluid">
+        © 2018 Copyright: <a href="http://www.MDBootstrap.com"> MDBootstrap.com </a>
+
+      </div>
+    </div>
+    <!--/.Copyright-->
+
+  </footer>
+  <!--/.Footer-->
 
 
-   <td class="alink">
-      <a href="/notable.html" title="That's Notable!">Notable Games</a>
-   </td>
-
-        <td class="alink"  >
-      <a href="http://cross-tables.com/results.php?playerid=20411" title="Get to know me">About Me</a>
-   </td>
-  </tr>
-  </tbody>
-    </table>
-    </td>
-   </tr>
-  </tbody>
- </table>
-</div>
-
-<div class="customer-logos">
-  <div class="slide">Yeet 1</div>
-  <div class="slide">Yeet 2</div>
-  <div class="slide">Yeet 3</div>
-  <div class="slide">Yeet 4</div>
-  <div class="slide">Yeet 5</div>
-  <div class="slide">Yeet 6</div>
-  <div class="slide">Yeet 7</div>
-</div>
 
 
-<div id="container" class="container">
-<div class="minegcgform">
-<form action="$cgibin_name/mine_webapp.pl" target="_blank" method="get" onsubmit="return nocacheresult()">
-
-  <table class="minegcgforminput">
-  <tbody id="$search_data_id">
-
-  </tbody>
-  </table>
-
-  <div class="minegcgform"><input type="submit" value="Submit" class="minegcgforminput"></div>
-
-
-</form>
-</div>
-
-</div>
+  <!-- SCRIPTS -->
+  <!-- JQuery -->
+  <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+  <!-- Bootstrap tooltips -->
+  <script type="text/javascript" src="js/popper.min.js"></script>
+  <!-- Bootstrap core JavaScript -->
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+  <!-- MDB core JavaScript -->
+  <script type="text/javascript" src="js/mdb.min.js"></script>
 </body>
+
 </html>
+
 
 HTML
 ;
