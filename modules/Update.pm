929cf16d96c1fef3a46a7e99ed6892bf531f72d4
Joshua Castellano <castellaj@VM-kellytg.corp.leidos.com>
2019-09-19 17:37:32 -0400

    Escaped dollars signs

--- modules/Update.pm
+++ modules/Update.pm
@@ -811,15 +811,15 @@ sub update_html
 
     \$.fn.shuffleChildren = function() {
         \$.each(this.get(), function(index, el) {
-            var $el = $(el);
-            var $find = $el.children();
+            var \$el = \$(el);
+            var \$find = \$el.children();
 
-            $find.sort(function() {
+            \$find.sort(function() {
                 return 0.5 - Math.random();
             });
 
-            $el.empty();
-            $find.appendTo($el);
+            \$el.empty();
+            \$find.appendTo(\$el);
         });
     };
 
