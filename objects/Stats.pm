7b72f100daaed87a2eeb34b0f124d85ada598390
Joshua Castellano <castellaj@VM-0.corp.leidos.com>
2019-09-25 16:56:03 -0400

    Style changes

--- objects/Stats.pm
+++ objects/Stats.pm
@@ -678,7 +678,7 @@ TABLE
   
     my $expander = make_expander($expander_id);
   
-    $content .= "<div><tr><td>$expander</td><td>$title</td></tr><tr><td></td><td>$list_table</td></tr></div>";
+    $content .= "<div><table><tbody><tr><td>$expander</td><td>$title</td></tr><tr><td></td><td>$list_table</td></tr></tbody></table></div>";
   }
   if (!$content)
   {
