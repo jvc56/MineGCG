2719e4e5335c1dc473bc13b1c9dd9417b3839937
Joshua Castellano <castellaj@VM-0.corp.leidos.com>
2019-09-27 18:20:06 -0400

    Tried importing more table scripts

--- objects/Stats.pm
+++ objects/Stats.pm
@@ -779,7 +779,7 @@ sub statListToHTML
     $expander_id =~ s/\s//g;
     my $list_table = <<TABLE
     <div class="collapse" id="$expander_id">
-      <table class="table table-striped table-bordered" cellspacing="0" width="100%">
+      <table class="display" cellspacing="0" width="100%">
         <thead>
           <tr>
             <th>Type</th>
