d67570486d47fca5ed993d5b3635d682b0110100
Joshua Castellano <joshuacastellano7@gmail.com>
2020-10-23 13:56:02 +0000

    Removed the evil

--- modules/Constants.pm
+++ modules/Constants.pm
@@ -952,18 +952,6 @@ use constant HTML_HEAD_CONTENT => <<HEAD
 
   <link href="/css/style.css" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css?family=VT323" rel="stylesheet">
-
-  <!-- Global site tag (gtag.js) - Google Analytics -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-153524496-1"></script>
-  <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-  
-    gtag('config', 'UA-153524496-1');
-  </script>
-  
-
 HEAD
 ;
 
